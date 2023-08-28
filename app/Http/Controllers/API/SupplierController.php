6124c564520b10e21d8fda9711c27d51b562f7bd
AzzadCodeBrain <azzad@coddedbrain.com>
2023-08-28 14:47:13 +0530

    update in validation

--- app/Http/Controllers/API/SupplierController.php
+++ app/Http/Controllers/API/SupplierController.php
@@ -111,22 +111,22 @@ class SupplierController extends Controller
             "fax_number" => $request->Fax,
             "country_code" => "+91",
             "number" => $request->Mobile,
-            "website" => $request->Website, // null
-            "company_nature" => $request->NatureOfCompany, //null
-            "business_nature" => $request->NatureOfBusiness, //null
-            "is_iso_resgitered" => $request->IsCertified ? "yes" : "no" , //null
-            "related_working"   => $request->RelativeWorkingWithUs, //null
+            "website" => $request->Website, 
+            "company_nature" => $request->NatureOfCompany, 
+            "business_nature" => $request->NatureOfBusiness, 
+            "is_iso_resgitered" => $request->IsCertified ? "yes" : "no" ,
+            "related_working"   => $request->RelativeWorkingWithUs, 
             "company_size"  => $request->SizeOfCompany,
             "established_year"  => $request->YearOfEstablishment,
-            "investment_value"  => $request->InvestmentValue,  //null
+            "investment_value"  => $request->InvestmentValue,  
             "registration_number"   => $request->RegistrationNo,
             "registration_date" => $request->RegistrationDate,
             "gst_number"    => $request->GSTRegistrationNo,
             "gst_reg_date"  => $request->GSTRegistrationDate,
             "pan"   => $request->PANNo,
             "pan_reg_date"  => $request->PANRegistrationDate,
-            "receipt_date"  => $request->ExpectedDateOfCertificateRecieve,  //null
-            "have_certification_copy" => 'no'  //null
+            "receipt_date"  => $request->ExpectedDateOfCertificateRecieve,  
+            "have_certification_copy" => 'no'  
         ]);
 
 
