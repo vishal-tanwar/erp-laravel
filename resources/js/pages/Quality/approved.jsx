c40b224f76827d1183b1559aa2a70afb3027423c
vishal-tanwar <codageui@gmail.com>
2023-10-12 18:26:53 +0530

    newchangesdone

--- resources/js/pages/Quality/approved.jsx
+++ resources/js/pages/Quality/approved.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../../partials/Layout";
-import { Form, Col, InputGroup, Row, Dropdown, Button } from "react-bootstrap";
+import { Form, Col, InputGroup, Row, Button } from "react-bootstrap";
 import { MdOutlinePrint, MdOutlineSearch } from "react-icons/md";
 
 
@@ -10,7 +10,7 @@ export default function Approved() {
     const [vouchers, setVouchers] = useState([]);
 
     return (
-        <Layout title="Approved">
+        <Layout title=" Quality Approved">
             <Row className="mb-4">
 
                 <Col xs={12}>
@@ -46,7 +46,7 @@ export default function Approved() {
 
                         <tr>
                             <td><Form.Check /></td>
-                            <td>Lorem</td>
+                            <td>1</td>
                             <td>Lorem</td>
                             <td>Lorem</td>
                             <td>Lorem</td>
